716528f2d2d78e297035ddaeb481ae659da31d86
RoscoeWelch <rwelch9@msudenver.edu>
2018-10-12 19:56:08 -0600

    Added comment of function with a for loop instead of title method.

--- pa2_main.py
+++ pa2_main.py
@@ -7,6 +7,11 @@ if __name__ == "__main__":
         new_string = " ".join(my_list).title()
         return new_string
 
-    # Prints capitalized list as string.
+    # Prints capitalized string.
     print(cap_join(my_list))
 
+    # In case a loop was wanted instead of the title method.
+
+    # def cap_join(my_list):
+    #     new_string = " ".join([my_list[word].capitalize() for word in range(len(my_list))])
+    #     return new_string
